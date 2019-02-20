55481d783f7b99b06154ddd8d4859fe3ab7499b0
ZeroShwarcz <45811334+ZeroShwarcz@users.noreply.github.com>
2019-02-20 16:59:44 +0300

    Add files via upload

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,14 +1,11 @@
 #include<iostream>
+#include<cmath>
 
 double my_sqrt(double a)
 {
         if(a==0)
                 return a;
-        if(a<1)
-                return 1/my_sqrt(1/a);
         double x = 1.0;
-        while(x*x<a)
-                x*=10;
         for(int i(0); i<10; ++i)
                 x=0.5*(x+a/x);
         return x;
@@ -19,6 +16,7 @@ int main()
         double sqrt=0;
         std::cout<<"Введите число, корень которого хотите вычислить: ";
         std::cin>>sqrt;
+	if(sqrt<0) sqrt*=-1;
         std::cout<<"Корень = "<< my_sqrt(sqrt)<<std::endl;
         return 0;
 }
