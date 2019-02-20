c6ea9029e5f0c38f207a7d579c5255057aa4ae01
ZeroShwarcz <45811334+ZeroShwarcz@users.noreply.github.com>
2019-02-20 16:59:24 +0300

    Add files via upload

--- poly.cpp
+++ poly.cpp
@@ -1,19 +1,30 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
 
-double poly(std::vector<int> poly)
+using namespace std;
+
+double poly(vector<int> poly)
 {
-        double sum = 0;
-        for(int i(0); i<poly.size();i++)
-        {
-                sum += 1.0/poly[i]*3.0;
-        }
-                return sum;
+    double sum = 0;
+    for(int i(0); i<poly.size();i++)
+    {
+        sum += 1.0/poly[i]*3.0;
+    }
+    return sum;
 }
+
 
 int main(int argc, char* argv[])
 {
-        std::vector<int> vec(5,2);
-        std::cout<< poly(vec) <<std::endl;
-        return 0;
+    string s="-poly";
+    vector <int> vec;
+if(argv[1]==s)       //первый флаг
+    for (int i(2);i<argc;i++)       //значения после флага
+        vec.push_back(atoi(argv[i]));   //пореобразование строки в число
+    //for (int i(0);i<vec.size();i++)  //вывод вектора
+        cout<<poly(vec)<<endl;
+
+    return 0;
 }
