09da0e8c31fbb04c5fb74947c60c3bc82b7ee22f
Zero <zero_shwarcz@mail.ru>
2019-02-24 09:43:48 +0300

    Mew

--- sqrt.cpp
+++ sqrt.cpp
@@ -3,6 +3,7 @@
 
 double my_sqrt(double a)
 {
+	      if(a<0) a*=-1;
         if(a==0)
                 return a;
         double x = 1.0;
@@ -16,7 +17,6 @@ int main()
         double sqrt=0;
         std::cout<<"Введите число, корень которого хотите вычислить: ";
         std::cin>>sqrt;
-	if(sqrt<0) sqrt*=-1;
         std::cout<<"Корень = "<< my_sqrt(sqrt)<<std::endl;
         return 0;
 }
